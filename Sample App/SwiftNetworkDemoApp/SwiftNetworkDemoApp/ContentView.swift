26f8d7a9510619d4efe3afa2584c801071f16d39
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2026-01-12 20:47:24 +0100

    chore(app): created project

---
+++ Sample App/SwiftNetworkDemoApp/SwiftNetworkDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftNetworkDemoApp
+//
+//  Created by Erik Sebastian de Erice Jerez on 12/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sample App/SwiftNetworkDemoApp/SwiftNetworkDemoApp/SwiftNetworkDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNetworkDemoAppApp.swift
+//  SwiftNetworkDemoApp
+//
+//  Created by Erik Sebastian de Erice Jerez on 12/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNetworkDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
