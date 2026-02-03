aab023b167d5e2a7be2eeb8af539001219e4bb26
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2026-02-03 16:48:31 +0100

    feat: init project

---
+++ Sample App/SwiftNetworkSampleApp/SwiftNetworkSampleApp/SwiftNetworkSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNetworkSampleAppApp.swift
+//  SwiftNetworkSampleApp
+//
+//  Created by Erik Sebastian de Erice Jerez on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNetworkSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
